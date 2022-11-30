20e2518fa5bf60db552f69779fd9cad068dff00e
roni-mazor <ro6596@gmail.com>
2022-11-30 19:54:34 +0200

    pp

--- middlewares/setupAls.middleware.ts
+++ middlewares/setupAls.middleware.ts
@@ -1,21 +1,21 @@
 export { }
 
-// const authService = require('../api/auth/auth.service')
-// const asyncLocalStorage = require('../services/als.service')
+const authService = require('../api/auth/auth.service')
+const asyncLocalStorage = require('../services/als.service')
 
-// async function setupAsyncLocalStorage(req: { cookies: { loginToken: any } }, res: any, next: () => void) {
-//     const storage = {}
-//     asyncLocalStorage.run(storage, () => {
-//         if (!req.cookies) return next()
-//         const loggedinUser = authService.validateToken(req.cookies.loginToken)
+async function setupAsyncLocalStorage(req: { cookies: { loginToken: string } }, res: any, next: () => void) {
+    const storage = {}
+    asyncLocalStorage.run(storage, () => {
+        if (!req.cookies) return next()
+        const loggedinUser = authService.validateToken(req.cookies.loginToken)
 
-//         if (loggedinUser) {
-//             const alsStore = asyncLocalStorage.getStore()
-//             alsStore.loggedinUser = loggedinUser
-//         }
-//         next()
-//     })
-// }
+        if (loggedinUser) {
+            const alsStore = asyncLocalStorage.getStore()
+            alsStore.loggedinUser = loggedinUser
+        }
+        next()
+    })
+}
 
-// module.exports = setupAsyncLocalStorage
+module.exports = setupAsyncLocalStorage
 
