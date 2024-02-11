5a66862ed2f152a8c835aee4756f1842fc8ee7e9
YoavSher <yoav.sher11@gmail.com>
2024-02-11 12:45:38 +0200

    updating cloudinary account

--- dist/server.js
+++ dist/server.js
@@ -12,12 +12,14 @@ app.use(cookieParser());
 app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.resolve(__dirname, 'public')));
+    console.log('prod');
 }
 else {
     const corsOptions = {
         origin: ['http://127.0.0.1:5173', 'http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
         credentials: true
     };
+    console.log('dev');
     app.use(cors(corsOptions));
 }
 const playlistRoutes = require('./api/playlist/playlist.routes');
